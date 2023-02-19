0447643beedd7928c04444968b3e9b9f349f950a
K S Kishore Babu <kishore.sai37@gmail.com>
2023-02-19 15:30:54 +0530

    add saas

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
-import './App.css';
+import React from 'react';
 
-function App() {
+// import './categories-styles.scss';
+
+const categories = [
+  { id: 1, title: 'Hats' },
+  { id: 2, title: 'Jackets' },
+  { id: 3, title: 'Sneakers' },
+  { id: 4, title: 'Women' },
+  { id: 5, title: 'Men' },
+];
+
+const App = () => {
   return (
-    <div className='App'>
-      <header>
-        <h1>Hello React!!!</h1>
-      </header>
+    <div className='categories-container'>
+      {categories.map(({ id, title }) => {
+        return (
+          <div
+            className='category-container'
+            key={id}
+          >
+            {/* <img src="" alt="" /> */}
+            <div className='category-details-container'>
+              <h2>{title}</h2>
+              <p>Shop Now</p>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
-}
+};
 
 export default App;
